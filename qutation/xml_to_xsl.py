d10e10812fb682a39041d34884f87e28c191963b
PSI <tkddls8848@naver.com>
2022-10-19 00:55:47 +0900

    complete xml to excel format

--- qutation/xml_to_xsl.py
+++ qutation/xml_to_xsl.py
@@ -18,41 +18,44 @@ def save_excel():
     w_book = Workbook()
     w_sheet = w_book.active
     w_sheet.cell(row=1, column=1).value = 'START'
-    for _ in root.getroot().iter():
-        product_quantity = 0
-        product_id = []
-        product_cost = []
-        if _.tag == "ProductSubLineItem":
-            print("ProductSubLineItem", i)
-        if _.tag == "Quantity":
-            print()
-            print(_.tag, _.text)
-            product_quantity = int(_.text)
-            print(product_quantity)
-        elif _.tag == "ProductIdentification":
-            for i in _.iter():
-                if i.tag == "ProprietaryProductIdentifier":
-                    print("Model No.", i.text)
-                    product_id.append(i.text)
-                elif i.tag == "ProductDescription":
-                    print("Description", i.text)
-                    product_id.append(i.text)
-                elif i.tag == "ProductTypeCode":
-                    print("ITEM", i.text)    
-                    product_id.append(i.text)                                   
-        elif _.tag == "UnitListPrice":
-            for i in _.find("FinancialAmount"):
-                if i.tag == "MonetaryAmount":
-                    print("cost", i.text)
-                    product_cost.append(i.text)
-        elif _.tag == "MaintenanceUnitListPrice":
-            print("MaintenanceUnitListPrice", _)   
-            product_cost.append(_.text)     
-        if product_id != []:
-            print(product_id)
-        if product_cost != []:
-            print(product_cost)
-    
+
+    MAIN_product_data, MAIN_product_cost = [], []
+    SUB_products_data, SUB_products_cost = [], []
+    for main in root.getroot().find("CFData").findall("ProductLineItem"):
+        main_product_id = main.find("ProductIdentification").find("PartnerProductIdentification")
+        main_product_u = main.find("UnitListPrice").find("FinancialAmount")
+        sub_products = main.findall("ProductSubLineItem")
+
+
+        for a in main_product_id.iter():
+            if a.tag == "ProprietaryProductIdentifier":
+                print("M ID", a.tag, a.text)
+                MAIN_product_data.append(a.text)
+        for a in main_product_u.find("MonetaryAmount").iter():
+            if a.tag == "MonetaryAmount":
+                print("M ID", a.tag, a.text)
+                MAIN_product_data.append(a.text)
+
+        for sub_product in sub_products:
+            SUB_product_data, SUB_product_cost = [], []
+            for a in sub_product.iter():
+                if a.tag == "Quantity":
+                    pass
+                if a.tag == "ProductIdentification":
+                    for b in a.find("PartnerProductIdentification"):
+                        print("S INFO", b.tag, b.text)
+                        SUB_product_data.append(b.text)
+                if a.tag == "UnitListPrice":
+                    for b in a.find("FinancialAmount"):
+                        print("S COST", b.tag, b.text)
+                        SUB_product_cost.append(b.text)
+            SUB_products_data.append(SUB_product_data)
+            SUB_products_cost.append(SUB_product_cost)
+        print(MAIN_product_data)
+        print(SUB_products_data)
+    w_sheet.append(MAIN_product_data)
+    for p in SUB_products_data:
+        w_sheet.append(p)
     w_book.save(os.getcwd() + '\\qutation\\tmp\\samples.xlsx')
 
 save_excel()
